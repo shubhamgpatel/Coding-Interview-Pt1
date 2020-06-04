d788475efd128c446f2e209b38fa2d57aaae74ec
Shubham <Shubham@LENOVO-PC>
2020-06-04 09:58:01 -0400

    Code optimized

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ function empfuntn(employees){
     let html = '';	//to write all data 
         let empobjects = Object.values(employees);
         empobjects.forEach(empobject => {  //for each too displayemployees
+		let empNameLength = empobject.employeefname.length + empobject.employeelname.length; // check the length of employeename and lastname to write on next line
         let roleDet = Object.values(empobject.roles);   // to convert from object into array
-
+		
             html += "<div class='card'>";
              html += "<div>"+ (empobject.employeeisfeatured == '1' ? "<img src='crown.png' class='crown'/>" : "") +"</div>";
             html += "<div><img src='http://sandbox.bittsdevelopment.com/code1/employeepics/" + empobject.employeeid +".jpg' alt='image' title='Employee' class='img-employee'/></div>";
-            html += "<div><h2>" + empobject.employeefname + " " + empobject.employeelname +"</h2></div>";
+            html += "<div><h2>" + ((empNameLength < 16) ? (empobject.employeefname.concat(" ",empobject.employeelname)) : (empobject.employeefname.concat("<br>",empobject.employeelname))) + "</h2></div>"; 
             html += "<div><p>" + empobject.employeebio +"</p></div>";
             roleDet.forEach(rolecontent => {
-
             html += "<span class='rolecolor' style='background: "+ rolecontent.rolecolor + "'>"+ rolecontent.rolename + "</span> "
         });
             html += "</div>";
